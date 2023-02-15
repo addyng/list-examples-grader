bc87a2b04f46bf7e8b1b632bd34a77b49bc3d253
addyng <110417388+addyng@users.noreply.github.com>
2023-02-15 13:18:57 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -3,3 +3,15 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if [[-f ListExamples.java]]
+then
+    echo "ListExamples found"
+else
+    echo "need file ListExamples.java"
+    exit 1
+fi
+
+cp ../ListExamples.java ./
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2> compileError.txt
